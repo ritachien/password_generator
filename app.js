62e5d6087e69e6c1fcfb19ff1517b106e5fd6402
ritachien <ritachien929@gmail.com>
2022-03-31 17:09:16 +0800

    feat: set body-parser & post method

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
-// Set express engine
+// Set template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// Set body-parser
+app.use(express.urlencoded({ extended: true }))
 
 // Route settings
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  res.render('index')
+  console.log(req.body)
 })
 
 // Listening server on localhost:3000
